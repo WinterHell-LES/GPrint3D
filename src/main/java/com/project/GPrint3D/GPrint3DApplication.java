39f64964638d46173f83ebeedc1ee82b69d71022
Fabricio <fabricio_lima_carvalho@hotmail.com>
2021-02-16 16:51:01 -0300

    Primeiro commit

---
+++ src/main/java/com/project/GPrint3D/GPrint3DApplication.java
@@ -0,0 +1,13 @@
+package com.project.GPrint3D;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPrint3DApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GPrint3DApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/GPrint3D/GPrint3DApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GPrint3D;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPrint3DApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

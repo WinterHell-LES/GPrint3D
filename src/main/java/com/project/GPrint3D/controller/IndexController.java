d617a1987f22d96c8b331eec7e96134e8a2ab1fd
Fabricio <fabricio_lima_carvalho@hotmail.com>
2021-02-16 23:44:48 -0300

    Controllers iniciais

---
+++ src/main/java/com/project/GPrint3D/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.project.GPrint3D.controller;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController 
+{
+    @RequestMapping({"/", "/index"})
+    public ModelAndView index(HttpServletRequest auth, Principal principal)
+    {
+        ModelAndView mv = new ModelAndView("/index");
+
+        return mv;
+    }
+}

21b84f4c02ff725a910a05c82720308cd3ae4dc5
George Wurthmann <george.wurthmann@fatec.sp.gov.br>
2021-02-28 15:50:36 -0300

    Data insert correção

--- Data_insert.sql
+++ Data_insert.sql
@@ -8,15 +8,15 @@ values ('admin@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJz
 -- Clientes
 -- Usuario - Cliente 1
 insert into usuarios (usu_email, usu_senha, usu_regra, usu_ativo)
-values ('cliente1@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLIENTE', true);
+values ('cliente1@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLI', true);
 
 -- Cliente
-insert into clientes (cli_tipo, cli_nome, cli_sobrenome, cli_sexo, cli_dtnasc, cli_usu_id)
-values ('Bronze', 'Sérgio Levi', 'Yuri Melo', 'Masculino', '1945-09-23', '2');
+insert into clientes (cli_ranking, cli_nome, cli_sexo, cli_dtnasc, cli_usu_id)
+values ('Bronze', 'Sérgio Levi Yuri Melo', 'Masculino', '1945-09-23', '2');
 
 -- Documento
-insert into documentos (doc_nome, doc_numero, doc_validade, doc_cli_id)
-values ('CPF', '91294168509', null, '1');
+insert into documentos (doc_tipo, doc_numero, doc_cli_id)
+values ('CPF', '91294168509', '1');
 
 -- Endereço
 insert into enderecos (end_entrega, end_cobranca, end_descricao, end_tipolog, end_logradouro, end_numero, end_cep, end_complemento, end_cidade, end_estado, end_pais, end_observacao, end_cli_id)
@@ -32,15 +32,15 @@ values ('Comercial', '67', '36975899', '1');
 
 -- Usuario - Cliente 2
 insert into usuarios (usu_email, usu_senha, usu_regra, usu_ativo)
-values ('cliente2@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLIENTE', true);
+values ('cliente2@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLI', true);
 
 -- Cliente
-insert into clientes (cli_tipo, cli_nome, cli_sobrenome, cli_sexo, cli_dtnasc, cli_usu_id)
-values ('Bronze', 'Cauã', 'Luiz Lima', 'Masculino', '1954-09-21', '3');
+insert into clientes (cli_ranking, cli_nome, cli_sexo, cli_dtnasc, cli_usu_id)
+values ('Bronze', 'Cauã Luiz Lima', 'Masculino', '1954-09-21', '3');
 
 -- Documento
-insert into documentos (doc_nome, doc_numero, doc_validade, doc_cli_id)
-values ('CPF', '03784356397', null, '2');
+insert into documentos (doc_tipo, doc_numero, doc_cli_id)
+values ('CPF', '03784356397', '2');
 
 -- Endereço
 insert into enderecos (end_entrega, end_cobranca, end_descricao, end_tipolog, end_logradouro, end_numero, end_cep, end_complemento, end_cidade, end_estado, end_pais, end_observacao, end_cli_id)
@@ -56,15 +56,15 @@ values ('Celular', '41', '987211324', '2');
 
 -- Usuario - Cliente 3
 insert into usuarios (usu_email, usu_senha, usu_regra, usu_ativo)
-values ('cliente3@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLIENTE', true);
+values ('cliente3@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLI', true);
 
 -- Cliente
-insert into clientes (cli_tipo, cli_nome, cli_sobrenome, cli_sexo, cli_dtnasc, cli_usu_id)
-values ('Bronze', 'Renato Nicolas', 'Lorenzo Araújo', 'Masculino', '1945-03-20', '4');
+insert into clientes (cli_ranking, cli_nome, cli_sexo, cli_dtnasc, cli_usu_id)
+values ('Bronze', 'Renato Nicolas Lorenzo Araújo', 'Masculino', '1945-03-20', '4');
 
 -- Documento
-insert into documentos (doc_nome, doc_numero, doc_validade, doc_cli_id)
-values ('CPF', '35308521719', null, '3');
+insert into documentos (doc_tipo, doc_numero, doc_cli_id)
+values ('CPF', '35308521719', '3');
 
 -- Endereço
 insert into enderecos (end_entrega, end_cobranca, end_descricao, end_tipolog, end_logradouro, end_numero, end_cep, end_complemento, end_cidade, end_estado, end_pais, end_observacao, end_cli_id)
@@ -80,15 +80,15 @@ values ('Comercial', '65', '36706873', '3');
 
 -- Usuario - Cliente 4
 insert into usuarios (usu_email, usu_senha, usu_regra, usu_ativo)
-values ('cliente4@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLIENTE', true);
+values ('cliente4@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLI', true);
 
 -- Cliente
-insert into clientes (cli_tipo, cli_nome, cli_sobrenome, cli_sexo, cli_dtnasc, cli_usu_id)
-values ('Bronze', 'Theo Heitor Henrique', 'da Paz', 'Masculino', '1975-06-14', '5');
+insert into clientes (cli_ranking, cli_nome, cli_sexo, cli_dtnasc, cli_usu_id)
+values ('Bronze', 'Theo Heitor Henrique da Paz', 'Masculino', '1975-06-14', '5');
 
 -- Documento
-insert into documentos (doc_nome, doc_numero, doc_validade, doc_cli_id)
-values ('CPF', '58748412708', null, '4');
+insert into documentos (doc_tipo, doc_numero, doc_cli_id)
+values ('CPF', '58748412708', '4');
 
 -- Endereço
 insert into enderecos (end_entrega, end_cobranca, end_descricao, end_tipolog, end_logradouro, end_numero, end_cep, end_complemento, end_cidade, end_estado, end_pais, end_observacao, end_cli_id)
@@ -104,15 +104,15 @@ values ('Celular', '27', '995943161', '4');
 
 -- Usuario - Cliente 5
 insert into usuarios (usu_email, usu_senha, usu_regra, usu_ativo)
-values ('cliente5@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLIENTE', true);
+values ('cliente5@gprint3d.com', '$2a$10$TMfY1IunLWQy/wfKgltNZ.jyaJObeOdAfBK2VPicJzVh10P0nnDQO', 'ROLE_CLI', true);
 
 -- Cliente
-insert into clientes (cli_tipo, cli_nome, cli_sobrenome, cli_sexo, cli_dtnasc, cli_usu_id)
-values ('Bronze', 'Calebe Mateus', 'Daniel Barbosa', 'Masculino', '1955-03-10', '6');
+insert into clientes (cli_ranking, cli_nome, cli_sexo, cli_dtnasc, cli_usu_id)
+values ('Bronze', 'Calebe Mateus Daniel Barbosa', 'Feminino', '1955-03-10', '6');
 
 -- Documento
-insert into documentos (doc_nome, doc_numero, doc_validade, doc_cli_id)
-values ('CPF', '47196638344', null, '5');
+insert into documentos (doc_tipo, doc_numero, doc_cli_id)
+values ('CPF', '47196638344', '5');
 
 -- Endereço
 insert into enderecos (end_entrega, end_cobranca, end_descricao, end_tipolog, end_logradouro, end_numero, end_cep, end_complemento, end_cidade, end_estado, end_pais, end_observacao, end_cli_id)

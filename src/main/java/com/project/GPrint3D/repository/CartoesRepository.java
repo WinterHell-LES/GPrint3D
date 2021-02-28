538a84e705470907635345aa0d97b1bf90894a4a
George Wurthmann <george.wurthmann@fatec.sp.gov.br>
2021-02-28 13:23:07 -0300

    Ajustes no cliente

--- src/main/java/com/project/GPrint3D/repository/CartoesRepository.java
+++ src/main/java/com/project/GPrint3D/repository/CartoesRepository.java
@@ -3,8 +3,11 @@ package com.project.GPrint3D.repository;
 import com.project.GPrint3D.model.CartoesModel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartoesRepository extends JpaRepository<CartoesModel, Integer>
 {
-
+    // Procura por nome do cliente
+    @Query(value = "SELECT * FROM cartoes WHERE crt_cli_id = ?", nativeQuery = true)
+    public CartoesModel findByClienteId(Integer id);
 }
--- src/main/java/com/project/GPrint3D/repository/TelefonesRepository.java
+++ src/main/java/com/project/GPrint3D/repository/TelefonesRepository.java
@@ -3,8 +3,11 @@ package com.project.GPrint3D.repository;
 import com.project.GPrint3D.model.TelefonesModel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TelefonesRepository extends JpaRepository<TelefonesModel, Integer>
 {
-
+    // Procura por nome do cliente
+    @Query(value = "SELECT * FROM telefones WHERE tel_cli_id = ?", nativeQuery = true)
+    public TelefonesModel findByClienteId(Integer id);
 }

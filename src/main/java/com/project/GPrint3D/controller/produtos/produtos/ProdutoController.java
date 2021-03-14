be0cd8e1cd8a69660ae6631ad95061c9e708827f
George Wurthmann <george.wurthmann@fatec.sp.gov.br>
2021-03-14 01:30:12 -0300

    Inclusão dos detalhes do produto

--- src/main/java/com/project/GPrint3D/controller/produtos/produtos/ProdutoController.java
+++ src/main/java/com/project/GPrint3D/controller/produtos/produtos/ProdutoController.java
@@ -2,13 +2,16 @@ package com.project.GPrint3D.controller.produtos.produtos;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ProdutoController 
 {
     @RequestMapping("/produto")
-    public String produto()
+    public ModelAndView produto()
     {
-        return "/produto";
+        ModelAndView mv = new ModelAndView("/produtos/descricao/descricaoProduto");
+
+        return mv;
     }
 }
